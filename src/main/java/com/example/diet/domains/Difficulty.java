01c35196b0eee170d71150586469cc8babf4e744
Monika Maria <monika.kocot9@gmail.com>
2019-03-16 14:24:32 +0100

    Initial commit

---
+++ src/main/java/com/example/diet/domains/Difficulty.java
@@ -0,0 +1,6 @@
+package com.example.diet.domains;
+
+public enum Difficulty {
+
+    EASY,MODERATE,KIND_OF_HARD,HARD
+}
---
+++ src/main/java/com/example/diet/domains/Ingredient.java
@@ -0,0 +1,43 @@
+package com.example.diet.domains;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Entity
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private BigDecimal amount;
+
+
+    public Ingredient() {
+    }
+
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom) {
+        this.description = description;
+        this.amount = amount;
+        this.uom = uom;
+    }
+
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom, Recipe recipe) {
+        this.description = description;
+        this.amount = amount;
+        this.uom = uom;
+        this.recipe = recipe;
+    }
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
+
+    @ManyToOne
+    private Recipe recipe;
+}
