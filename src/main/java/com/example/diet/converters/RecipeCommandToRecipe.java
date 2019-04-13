1d2ca8d140164da15de9da5e368328dc10d2d912
Monika Maria <monika.kocot9@gmail.com>
2019-04-13 14:38:47 +0200

    Converters package.

---
+++ src/main/java/com/example/diet/converters/RecipeCommandToRecipe.java
@@ -0,0 +1,53 @@
+package com.example.diet.converters;
+
+import com.example.diet.commands.RecipeCommand;
+import com.example.diet.domains.Recipe;
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RecipeCommandToRecipe implements Converter<RecipeCommand, Recipe> {
+
+    private final CategoryCommandToCategory categoryConveter;
+    private final IngredientCommandToIngredient ingredientConverter;
+
+
+    public RecipeCommandToRecipe(CategoryCommandToCategory categoryConveter, IngredientCommandToIngredient ingredientConverter) {
+        this.categoryConveter = categoryConveter;
+        this.ingredientConverter = ingredientConverter;
+    }
+
+    @Synchronized
+    @Nullable
+    @Override
+    public Recipe convert(RecipeCommand source) {
+        if (source == null) {
+            return null;
+        }
+
+        final Recipe recipe = new Recipe();
+        recipe.setId(source.getId());
+        recipe.setCookTime(source.getCookTime());
+        recipe.setPrepTime(source.getPrepTime());
+        recipe.setDescription(source.getDescription());
+        recipe.setDifficulty(source.getDifficulty());
+        recipe.setDirections(source.getDirections());
+        recipe.setServings(source.getServings());
+        recipe.setSource(source.getSource());
+        recipe.setUrl(source.getUrl());
+
+        if (source.getCategories() != null && source.getCategories().size() > 0){
+            source.getCategories()
+                    .forEach( category -> recipe.getCategories().add(categoryConveter.convert(category)));
+        }
+
+        if (source.getIngredients() != null && source.getIngredients().size() > 0){
+            source.getIngredients()
+                    .forEach(ingredient -> recipe.getIngredients().add(ingredientConverter.convert(ingredient)));
+        }
+
+        return recipe;
+    }
+}
