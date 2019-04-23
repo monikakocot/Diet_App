0f66eb61de109cb9715fadb8bb1c7de841ee4275
Monika Maria <monika.kocot9@gmail.com>
2019-04-23 16:19:04 +0200

    Showing details of Recipe.

---
+++ src/main/java/com/example/diet/controllers/RecipeController.java
@@ -0,0 +1,28 @@
+package com.example.diet.controllers;
+
+import com.example.diet.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Slf4j
+@Controller
+public class RecipeController {
+
+    private final RecipeService recipeService;
+
+    public RecipeController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @GetMapping("/recipe/{id}/show")
+    public String showById(@PathVariable String id, Model model){
+
+        model.addAttribute("recipe", recipeService.findById(new Long(id)));
+
+        return "recipe/show";
+    }
+
+}
